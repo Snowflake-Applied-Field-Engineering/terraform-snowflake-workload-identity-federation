f24d63f3ac1de098f06b380baa4ac7caaf67006d
Matt Barreiro <matt.barreiro+oss@snowflake.com>
2025-10-23 14:58:20 -0400

    simple aws example

--- examples/basic-aws-existing-role/README.md
+++ examples/basic-aws-existing-role/README.md
@@ -1,3 +1,74 @@
 # Example: AWS with Existing IAM Role
 
 This example shows how to configure an existing AWS role for WIF in Snowflake.
+<!-- # Basic Example
+
+This example demonstrates the basic usage of the Terraform module.
+
+## Usage
+
+1. **Configure variables**
+
+   Create a `terraform.tfvars` file:
+
+   ```hcl
+   name        = "my-resource"
+   environment = "dev"
+   region      = "us-east-1"
+
+   tags = {
+     Project   = "demo"
+     Owner     = "team@example.com"
+   }
+   ```
+
+2. **Initialize Terraform**
+
+   ```bash
+   terraform init
+   ```
+
+3. **Review the plan**
+
+   ```bash
+   terraform plan
+   ```
+
+4. **Apply the configuration**
+
+   ```bash
+   terraform apply
+   ```
+
+5. **Clean up**
+
+   ```bash
+   terraform destroy
+   ```
+
+## What This Example Creates
+
+This example creates:
+- [List the resources created]
+
+## Requirements
+
+| Name | Version |
+|------|---------|
+| terraform | >= 1.5.0 |
+| aws | ~> 5.0 |
+| snowflake | ~> 0.94 |
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| name | Name of the resource | `string` | `"basic-example"` | no |
+| environment | Environment name | `string` | `"dev"` | no |
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| id | The ID of the created resource |
+| name | The name of the created resource | -->
--- examples/basic-aws-existing-role/main.tf
+++ examples/basic-aws-existing-role/main.tf
@@ -42,24 +42,13 @@ module "wif_aws" {
 
   # Add your basic example here
   source = "../../"
-  # TODO Because examples will often be copied into other repositories for customization, any module blocks should have their source set to the address an external caller would use, not to a relative path.  https://developer.hashicorp.com/terraform/language/modules/develop/structure. BUT!
-  # ! BUT this will break github actions as it will always test the old version
 
-  #   source = "./modules/wif-aws-iam"
-  #   source = "git::https://github.com/Snowflake-Applied-Field-Engineering/terraform-snowflake-workload-identity-federation.git?ref=init"
+  csp          = "aws"
+  aws_role_arn = var.aws_role_arn
 
-  # source = "/Users/mbarreiro/src/terraform-snowflake-workload-identity-federation"
-
-  #   name_prefix = var.name_prefix
-  # tags = var.tags
-  wif_test_database     = var.wif_test_database
-  wif_role_name         = var.wif_role_name
-  wif_user_name         = var.wif_user_name
   wif_default_warehouse = var.wif_default_warehouse
+  wif_role_name         = var.wif_role_name
+  wif_test_database     = var.wif_test_database
   wif_test_schema       = var.wif_test_schema
-  aws_role_arn          = var.aws_role_arn
-  # azure_tenant_id = var.azure_tenant_id
-  # azure_sp_id = var.azure_sp_id
-  # gcp_project_id = var.gcp_project_id
-  # gcp_service_account_email = var.gcp_service_account_email
+  wif_user_name         = var.wif_user_name
 }
