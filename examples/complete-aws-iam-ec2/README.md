2aba0dc82e231805b2d17d351eccbffcd3ae5fe8
Matt Barreiro <matt.barreiro+oss@snowflake.com>
2026-05-07 10:50:23 -0400

    v0.2.0 - Make generic and composable

--- examples/complete-aws-iam-ec2/README.md
+++ examples/complete-aws-iam-ec2/README.md
@@ -11,3 +11,54 @@ This example creates:
 
 TODO - update above for accuracy
 <!-- TODO proper readme -->
+
+<!-- BEGIN_TF_DOCS -->
+## Requirements
+
+| Name | Version |
+|------|---------|
+| <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 1.5.0 |
+| <a name="requirement_aws"></a> [aws](#requirement\_aws) | >= 6.0, < 7.0 |
+| <a name="requirement_snowflake"></a> [snowflake](#requirement\_snowflake) | >= 2.9, <= 2.12 |
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| <a name="provider_aws"></a> [aws](#provider\_aws) | >= 6.0, < 7.0 |
+
+## Modules
+
+| Name | Source | Version |
+|------|--------|---------|
+| <a name="module_aws_iam_role"></a> [aws\_iam\_role](#module\_aws\_iam\_role) | terraform-aws-modules/iam/aws//modules/iam-role | n/a |
+| <a name="module_ec2_instance"></a> [ec2\_instance](#module\_ec2\_instance) | terraform-aws-modules/ec2-instance/aws | n/a |
+| <a name="module_snowflake_wif_role"></a> [snowflake\_wif\_role](#module\_snowflake\_wif\_role) | ../../ | n/a |
+
+## Resources
+
+| Name | Type |
+|------|------|
+| [aws_region.current](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/region) | data source |
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| <a name="input_ami_id"></a> [ami\_id](#input\_ami\_id) | Optional override AMI ID (use this if you have a golden image). If omitted, the latest x86\_64 AL2023 AMI will be used. | `string` | `null` | no |
+| <a name="input_instance_type"></a> [instance\_type](#input\_instance\_type) | EC2 instance type. | `string` | `"t3.micro"` | no |
+| <a name="input_key_name"></a> [key\_name](#input\_key\_name) | Optional EC2 key pair name (required if using SSH) | `string` | `null` | no |
+| <a name="input_name_prefix"></a> [name\_prefix](#input\_name\_prefix) | Prefix to apply to resource names | `string` | `"snow_tf_wif_test"` | no |
+| <a name="input_snowflake_account_name"></a> [snowflake\_account\_name](#input\_snowflake\_account\_name) | Snowflake account locator (e.g., xy12345 or xy12345.us-east-1) | `string` | n/a | yes |
+| <a name="input_snowflake_organization_name"></a> [snowflake\_organization\_name](#input\_snowflake\_organization\_name) | Snowflake org name | `string` | n/a | yes |
+| <a name="input_subnet_id"></a> [subnet\_id](#input\_subnet\_id) | Subnet ID for the test instance (private subnet recommended with SSM access) | `string` | n/a | yes |
+| <a name="input_tags"></a> [tags](#input\_tags) | Map of tags to apply to all AWS resources | `map(string)` | `{}` | no |
+| <a name="input_vpc_id"></a> [vpc\_id](#input\_vpc\_id) | Existing VPC ID where the test instance will run | `string` | n/a | yes |
+| <a name="input_wif_test_database"></a> [wif\_test\_database](#input\_wif\_test\_database) | Database to test privileges of the WIF test user/role(must exist) | `string` | `null` | no |
+| <a name="input_wif_test_schema"></a> [wif\_test\_schema](#input\_wif\_test\_schema) | Schema to test privileges of the WIF test user/role(must exist) | `string` | `null` | no |
+| <a name="input_wif_test_warehouse"></a> [wif\_test\_warehouse](#input\_wif\_test\_warehouse) | Warehouse for the WIF test user/role (must exist) | `string` | `null` | no |
+
+## Outputs
+
+No outputs.
+<!-- END_TF_DOCS -->
--- variables.tf
+++ variables.tf
@@ -2,13 +2,13 @@
 # General
 ################################################################################
 
-variable "csp" {
-  description = "Type of WIF identity to create"
+variable "wif_type" {
+  description = "The type of WIF identity to create. Must be one of: aws, azure, gcp, oidc."
   type        = string
   default     = "aws"
   validation {
-    condition     = contains(["aws", "azure", "gcp"], var.csp)
-    error_message = "Invalid WIF identity type: ${var.csp}. Valid types are: aws, azure, gcp."
+    condition     = contains(["aws", "azure", "gcp", "oidc"], var.wif_type)
+    error_message = "Invalid WIF type: ${var.wif_type}. Valid values are: aws, azure, gcp, oidc."
   }
 }
 
@@ -17,33 +17,29 @@ variable "csp" {
 ################################################################################
 
 variable "wif_role_name" {
-  description = "Name of the WIF test role"
+  description = "Name of the Snowflake role to create for WIF."
   type        = string
-  default     = "WIF_TEST_ROLE"
 }
 
 variable "wif_user_name" {
-  description = "Name of the WIF test user"
+  description = "Name of the Snowflake Service User to create for WIF."
   type        = string
-  default     = "WIF_TEST_USER"
 }
 
-variable "wif_default_warehouse" {
-  description = "Default warehouse for the WIF test user/role (must exist)"
+variable "wif_user_default_warehouse" {
+  description = "(Optional) Default warehouse for the WIF service user. Must already exist if provided (not validated by module)."
   type        = string
   default     = null
 }
 
-variable "wif_test_database" {
-  description = "Database to test privileges of the WIF test user/role(must exist)"
-  type        = string
-
-}
-
-variable "wif_test_schema" {
-  description = "Schema to test privileges of the WIF test user/role(must exist)"
-  type        = string
-  default     = null
+variable "wif_role_permissions" {
+  description = "A map of objects describing the custom permissions to grant to the WIF role. Note that for schemas, the name must be in DATABASE.SCHEMA format."
+  type = map(object({
+    type        = string       # one of "database", "schema", "warehouse"
+    name        = string       # name of the database, schema, or warehouse. Schema must be in DB.SCHEMA format.
+    permissions = list(string) # list of permissions to grant
+  }))
+  default = {}
 }
 
 ################################################################################
@@ -54,6 +50,13 @@ variable "aws_role_arn" {
   description = "ARN of the AWS role to use for WIF"
   type        = string
   default     = null
+  validation {
+    condition = var.aws_role_arn == null || can(regex(
+      "^arn:aws:(iam::[0-9]{12}:(user|role)/.+|sts::[0-9]{12}:assumed_role/.+/.+)$",
+      var.aws_role_arn
+    ))
+    error_message = "aws_role_arn must be a valid AWS ARN in one of these formats: arn:aws:iam::<account>:user/<user_name_with_path>, arn:aws:iam::<account>:role/<role_name_with_path>, or arn:aws:sts::<account>:assumed_role/<role_name>/<role_session_name>."
+  }
 }
 
 ################################################################################
@@ -66,23 +69,40 @@ variable "azure_tenant_id" {
   default     = null
 }
 
-variable "azure_sp_id" {
-  description = "Effective Azure SP ID"
+variable "azure_service_principal_id" {
+  description = "The case-sensitive Object ID (Principal ID) of the managed identity assigned to the Azure workload."
   type        = string
   default     = null
 }
-
 
 ################################################################################
 # GCP WIF variables
 ################################################################################
 
-# variable "gcp_wif_project_id" {
-#   description = "GCP project ID"
-#   type        = string
-# }
+variable "gcp_service_account_id" {
+  description = "The unique ID of the GCP service account to use for WIF"
+  type        = string
+  default     = null
+}
 
-# variable "gcp_wif_service_account_email_effective" {
-#   description = "Effective GCP WIF service account email"
-#   type        = string
-# }
+################################################################################
+# OIDC WIF variables
+################################################################################
+
+variable "oidc_issuer_url" {
+  description = "The OpenID Connect (OIDC) issuer URL."
+  type        = string
+  default     = null
+}
+
+variable "oidc_subject" {
+  description = "The identifier of the workload that is connecting to Snowflake. The format of the value is specific to the OIDC provider that is issuing the attestation."
+  type        = string
+  default     = null
+}
+
+variable "oidc_audience_list" {
+  description = "Specifies which values must be present in the aud claim of the ID token issued by the OIDC provider. Snowflake accepts the attestation if the aud claim contains at least one of the specified audiences."
+  type        = list(string)
+  default     = ["snowflakecomputing.com"]
+}
